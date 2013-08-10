1b0452831bfa1cc1bf8cf6a66c2bc112237f3a03
andrewmunro <amunro_92@hotmail.com>
2013-08-10 15:39:34 +0100

    Updating GameEvents

--- Assets/Scripts/GameEvents/GameEvent.cs
+++ Assets/Scripts/GameEvents/GameEvent.cs
@@ -9,9 +9,12 @@ namespace Assets.Scripts.GameEvents
 
     public class GameEvent : MonoBehaviour
     {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+
         void Start()
         {
-            GameManager.GetInstance.NotificationManager.ShowNotification();
+            GameManager.GetInstance.NotificationManager.ShowNotification(this);
         }
     }
 }
--- Assets/Scripts/Managers/GameEventManager.cs
+++ Assets/Scripts/Managers/GameEventManager.cs
@@ -1,23 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using Assets.Scripts.GameEvents;
 using UnityEngine;
+using Random = System.Random;
 
 namespace Assets.Scripts.Managers
 {
     public class GameEventManager : MonoBehaviour
     {
+        private List<Type> GameEvents;
 
         void Start()
         {
-            
+            GameEvents = typeof(GameEvent).Assembly.GetTypes().Where(t =>
+                t.IsClass && t.BaseType == typeof(GameEvent)
+                && Attribute.IsDefined(t, typeof(SerializableAttribute))).ToList();
+        }
+
+        public void StartEvent(Type gameEventType = null)
+        {
+            if (gameEventType != null)
+            {
+                
+            }
         }
 
         public void AddEventToGameObject<T>(GameObject gameObject) where T : GameEvent
         {
             gameObject.AddComponent<T>();
+        }
+
+        private List<T> GetGameEventsByType<T>() where T : GameEvent
+        {
+            return GameEvents.OfType<T>().ToList();
+        }
+
+        private GameEvent GetGameEventByRandom(List<Type> gameEvents)
+        {
+            Random randNum = new Random();
+            GameEvent gameEvent = (GameEvent) gameEvents[randNum.Next(gameEvents.Count)];
         }
     }
 }
