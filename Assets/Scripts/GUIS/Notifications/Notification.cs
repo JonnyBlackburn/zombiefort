7ab142030ecd3636bd80c174284a71a088a43c91
andrewmunro <amunro_92@hotmail.com>
2013-08-10 14:49:27 +0100

    Adding notifications.

---
+++ Assets/Scripts/GUIS/Notifications/Notification.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Scripts.GUI.Notifications;
+using UnityEngine;
+
+namespace Assets.Scripts.GUIComponents.Notifications
+{
+    public class Notification : MonoBehaviour
+    {
+        public string Title;
+        public string Body;
+        public Texture2D ImageType;
+        public NotificationButtons NotificationButtons;
+
+        private Rect windowRect;
+        private Vector2 scrollPosition = Vector2.zero;
+
+        void OnGUI()
+        {
+            windowRect = GUILayout.Window(0, windowRect, CreateWindow, Title ?? "Undefined");
+        }
+
+        void Awake()
+        {
+            windowRect = new Rect(Screen.width / 2 - 150, 150, 300, 100);
+        }
+
+        private void CreateWindow(int id)
+        {
+            GUILayout.TextArea(Body ?? "Undefined", )
+        }
+    }
+}
