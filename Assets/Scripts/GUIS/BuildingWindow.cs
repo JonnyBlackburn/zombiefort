94f35ed423c229213b7026f890ad991a23ef7134
Phil Allcock <phil_allcock@hotmail.com>
2013-08-10 17:18:46 +0100

    Fixed conflict

--- Assets/Scripts/GUIS/BuildingWindow.cs
+++ Assets/Scripts/GUIS/BuildingWindow.cs
@@ -3,22 +3,51 @@ using System.Collections;
 
 public class BuildingWindow : GUIBase {
 
-	int width = 400;
-	Building selectedBuilding;
+    int width = 400;
+    Building selectedBuilding;
 
-	public override void openView(object data)
-	{
-		selectedBuilding = data as Building;
-	}
+    public override void openView(object data)
+    {
+        selectedBuilding = data as Building;
+    }
 
 	void OnGUI()
-	{
-		GUILayout.BeginArea(new Rect(Screen.width - width, 0, width, Screen.height));
-		GUILayout.Box("", GUILayout.Height(Screen.height));
-		if (GUI.Button(new Rect (0, 0, 50, 20), "Close"))
-		{
-			GameManager.GetInstance.GuiManager.closeGUI(this.GetType().Name);
-		}
-		GUILayout.EndArea();
-	}
+    {
+        GUILayout.BeginArea(new Rect(Screen.width - width, 0, width, Screen.height));
+        //GUILayout.Box("", GUILayout.Height(Screen.height));
+        GUILayout.BeginVertical("box", GUILayout.Height(Screen.height));
+        if (GUILayout.Button("close", GUILayout.Width(50)))
+        {
+            GameManager.GetInstance.GuiManager.closeGUI(this.GetType().Name);
+        }
+        if (selectedBuilding.isWorkableBuilding) WorkableBuildingGUI();
+        else NonWorkableBuildingGUI();
+        
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
+
+    void WorkableBuildingGUI()
+    {
+        if (GUILayout.Button("Assign person"))
+        {
+            GameManager.GetInstance.GuiManager.openGUI("PeopleWindow", false);
+        }
+    }
+
+    void NonWorkableBuildingGUI()
+    {
+        if (selectedBuilding.CanAffordNexPhase())
+        {
+            if (GUILayout.Button("Upgrade"))
+            {
+                selectedBuilding.UpgradeBuilding();
+            }
+        }
+        else
+        {
+            GUILayout.Box("Can't afford upgrade");
+        }
+        GUILayout.Label("Building's health: " + selectedBuilding.buildingHealth);
+    }
 }
