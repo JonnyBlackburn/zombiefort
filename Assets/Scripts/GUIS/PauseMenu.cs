b7d4f3fb98c03793c4c6e3e05e5438704724e3a4
Phil Allcock <phil_allcock@hotmail.com>
2013-08-11 06:55:54 +0100

    Added pause menu

---
+++ Assets/Scripts/GUIS/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    bool isOpen = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) isOpen = !isOpen;
+    }
+
+    void OnGUI()
+    {
+        if (isOpen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "Exit"))
+            {
+                Application.Quit();
+            }
+        }
+    }
+	
+}
